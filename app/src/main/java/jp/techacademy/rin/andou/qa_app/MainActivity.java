0d3990e601f6499e0c3d0831d3fca8d344eabe6c
berukokoko <beru2491@gmail.com>
2018-09-16 18:55:03 +0900

    編集

--- app/src/main/java/jp/techacademy/rin/andou/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/rin/andou/qa_app/MainActivity.java
@@ -37,19 +37,28 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-    private Toolbar mToolbar;
-    private int mGenre = 0;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // --- ここから ---
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,18 +71,9 @@ public class MainActivity extends AppCompatActivity
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                 }
-
             }
         });
-
-        // ナビゲーションドロワーの設定
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // --- ここまで修正 ---
     }
 
     @Override
@@ -97,28 +97,4 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.nav_hobby) {
-            mToolbar.setTitle("趣味");
-            mGenre = 1;
-        } else if (id == R.id.nav_life) {
-            mToolbar.setTitle("生活");
-            mGenre = 2;
-        } else if (id == R.id.nav_health) {
-            mToolbar.setTitle("健康");
-            mGenre = 3;
-        } else if (id == R.id.nav_compter) {
-            mToolbar.setTitle("コンピューター");
-            mGenre = 4;
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-}
 }
